c1fe99e5de8b6f07571f3968280fa2b3ba376c64
SamuelGutierrez24 <1007370844@u.icesi.edu.co>
2023-09-11 10:30:50 -0500

    build: coding base

---
+++ Icesi_Students_Management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IcesiStudentsManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Icesi_Students_Management'
