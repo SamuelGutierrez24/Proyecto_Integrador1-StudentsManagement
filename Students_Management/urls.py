59a05b26ff40b66098a6ab26731517681968f964
Daniel Escobar <103274886+Pichi08@users.noreply.github.com>
2023-09-17 16:35:36 -0500

    feat: Base for signin, signup, logout and home

--- Students_Management/urls.py
+++ Students_Management/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Icesi_Students_Management import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('signin/', views.signin, name='singin'),
+    path('logout/', views.signout, name='logout'),
 ]
