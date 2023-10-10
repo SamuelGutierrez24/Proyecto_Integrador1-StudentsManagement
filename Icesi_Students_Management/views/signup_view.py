95dde3882ffa815e2bae0ed5c14503d9081d1842
Daniel Escobar <daniel.escobar6@u.icesi.edu.co>
2023-10-10 18:58:30 -0500

    Feat: abstract user, rol set to null when register and added new fields

--- Icesi_Students_Management/views/signup_view.py
+++ Icesi_Students_Management/views/signup_view.py
@@ -2,24 +2,26 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from Icesi_Students_Management.models import User
 from django.contrib.auth import login
 from django.db import IntegrityError
 
 
 def signup(request):
     if request.method == 'POST':
-        print("2")
-        if (request.POST.get('username')
+        if (request.POST.get('name')
+                and request.POST.get('lastName')
+                and request.POST.get('username')
                 and request.POST.get('email')
+                and request.POST.get('phoneNumber')
                 and request.POST.get('password1')
                 and request.POST.get('password2')):
-            print("3")
             if request.POST['password1'] == request.POST['password2']:
-                print("4")
                 try:
-                    user = User.objects.create_user(username=request.POST['username'],
-                                                    password=request.POST['password1'])
+                    user = User.objects.create_user(first_name=request.POST['name'], password=request.POST['password1'],
+                                                    email=request.POST['email'], is_active=False, is_staff=False,
+                                                    is_superuser=False, phone=request.POST['phoneNumber'],
+                                                    last_name=request.POST['lastName'], username=request.POST['username'])
                     user.save()
                     login(request, user)
                     messages.success(request, 'Cuenta creada satisfactoriamente! (espera a que sea activada)')
@@ -37,7 +39,6 @@ def signup(request):
                 "error": 'Todos los campos son requeridos'
             })
     else:
-        print("1")
         return render(request, 'signup.html', {
             'form': UserCreationForm
         })
