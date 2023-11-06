fa47924e3d76141b03e7b84766f18460f0de2003
SamuelGutierrez24 <1007370844@u.icesi.edu.co>
2023-11-05 22:31:57 -0500

    style: :lipstick: Better UI in the activity registration of BU

--- Icesi_Students_Management/views/bumenu.py
+++ Icesi_Students_Management/views/bumenu.py
@@ -8,12 +8,18 @@ def menu(request):
 
         notificaciones = Alerta.objects.all()
         notifi = []
+        history = HistoryActivityAssistance.objects.all()
+        histo = []
 
         for noti in notificaciones:
             if(noti.type==2):
                 notifi.append(noti)
+        
+        for histor in history:
+            if(histor.activity.tipo==1):
+                histo.append(histor)
 
-        return render(request, 'menuBU.html', {'notificaciones': notifi})
+        return render(request, 'menuBU.html', {'notificaciones': reversed(notifi), 'history': histo})
     
     else:
 
